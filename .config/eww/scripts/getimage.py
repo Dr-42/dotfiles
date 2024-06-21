3dc7b421ec269b133578f4971f77f897a1c61c69
Spandan Roy <royspandan1@gmail.com>
2024-06-22 00:30:01 +0530

    Immediate sync background and colors

--- .config/eww/scripts/getimage.py
+++ .config/eww/scripts/getimage.py
@@ -4,6 +4,7 @@
 
 import os
 import random
+import subprocess
 
 wallpapers_dir = os.path.expanduser("~/dotfiles/Wallpapers")
 
@@ -12,9 +13,20 @@ wallpaper = random.choice(wallpapers)
 
 complete_path = os.path.join(wallpapers_dir, wallpaper)
 
-print(complete_path)
-
 # save the wallpaper path in a file
 with open(os.path.expanduser("~/.cache/.wallpaper"), "w") as f:
     f.write(complete_path)
     f.close()
+
+# extract colors from the wallpaper
+css = subprocess.run(["scripts/getcss.py"], capture_output=True).stdout.decode("utf-8")
+# append " before and after the css
+# css = f'"{css}"'
+
+arg = f"getcss={css}"
+
+home = os.path.expanduser("~")
+eww_binary = os.path.join(home, ".local/bin/eww")
+subprocess.run([eww_binary, "update", arg])
+
+print(complete_path)
