a1ac1c01ee3cc67cdcd9590e40cc3b060f4a0c4c
Spandan Roy <royspandan1@gmail.com>
2024-07-28 12:35:59 +0530

    Update nvim plugins and switch to ayu

--- .config/nvim/lua/spandan/theme.lua
+++ .config/nvim/lua/spandan/theme.lua
@@ -2,14 +2,14 @@ local M = {}
 
 M.apply_theme = function()
 	-- vim.cmd [[colorscheme paper]]
-	-- vim.cmd [[colorscheme ayu]]
+	vim.cmd [[colorscheme ayu]]
 	-- vim.cmd [[colorscheme github_dark_dimmed]]
 	-- vim.cmd [[colorscheme catppuccin]]
 	-- vim.g.nightflyTransparent = true
 	-- vim.cmd [[colorscheme nightfly]]
 	-- vim.cmd [[colorscheme rose-pine]]
 	-- vim.cmd [[colorscheme kanagawa]]
-	vim.cmd [[colorscheme everforest]]
+	-- vim.cmd [[colorscheme everforest]]
 end
 
 return M
