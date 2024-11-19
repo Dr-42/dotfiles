d2e549dd2635573d1fcaa0aaa3b398a29dff5ad3
Spandan Roy <royspandan1@gmail.com>
2024-11-19 17:13:11 +0530

    Better hyrland setup and better eww bar window dodging

--- .config/eww/scripts/showbar.py
+++ .config/eww/scripts/showbar.py
@@ -3,7 +3,7 @@
 import subprocess
 import json
 
-hyprout = (
+hypr_ws = (
     subprocess.run("hyprctl activeworkspace -j", shell=True, capture_output=True)
     .stdout.decode("utf-8")
     .strip()
@@ -13,13 +13,31 @@ curpos = (
     .stdout.decode("utf-8")
     .strip()
 )
+hypr_wins = (
+    subprocess.run("hyprctl clients -j", shell=True, capture_output=True)
+    .stdout.decode("utf-8")
+    .strip()
+)
 
-hyprout = json.loads(hyprout)
+hypr_ws = json.loads(hypr_ws)
 curpos = json.loads(curpos)
+hypr_wins = json.loads(hypr_wins)
+
+
+def get_window_res(active_ws, y_thresh):
+    for window in hypr_wins:
+        if window["workspace"]["id"] == active_ws:
+            if window["at"][1] < y_thresh:
+                return False
+
+    return True
+
 
 if curpos["y"] < 30:
     print("true")
-elif hyprout["windows"] == 0:
+elif hypr_ws["windows"] == 0:
+    print("true")
+elif get_window_res(hypr_ws["id"], 40):
     print("true")
 else:
     print("false")
