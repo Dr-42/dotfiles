12c85517011d300b4e6d6a9d61f4f11842fc0089
Spandan Roy <royspandan1@gmail.com>
2024-12-27 16:49:21 +0530

    Add copilot chat and better status bar

--- .config/nvim/lua/spandan/plugins/copilot.lua
+++ .config/nvim/lua/spandan/plugins/copilot.lua
@@ -1,10 +1,24 @@
 return {
-  "github/copilot.vim",
-  event = "InsertEnter",
-  config = function()
-    -- Add current cwd to workspace folder of copilot
-    vim.cmd [[
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      -- Add current cwd to workspace folder of copilot
+      vim.cmd [[
       let g:copilot#workspace_folders = [getcwd()]
     ]]
-  end
+    end
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",                          -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
